70c7c5643e8ef223017beb61c411e2233c649c13
Sangmin Kim <offiksm0903@naver.com>
2024-12-10 12:28:00 +0900

    Initial Commit

---
+++ PracticeShareExtension/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeShareExtension
+//
+//  Created by 김상민 on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
